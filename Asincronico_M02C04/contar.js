7110587a25fb72bedce5558e638ae2f9edcc7374
CarolaineJacqueline <caritojac.campos@gmail.com>
2022-05-09 15:11:24 -0400

    up

---
+++ Asincronico_M02C04/contar.js
@@ -0,0 +1,12 @@
+function contarDeA_n(contar_de_a,contar_hasta ){  
+    let contador =contar_de_a;
+
+     while(contador <= contar_hasta){
+              console.log ( contador);
+
+         contador ++  
+    }      
+         
+     }   
+
+ contarDeA_n (7, 37)
---
+++ Asincronico_M02C04/fizzbuzz.js
@@ -0,0 +1,21 @@
+function varfizzbuzz  ( palabra1,palabra2) {
+    let contarHasta =num1,num2
+         let contador = 0;
+while(contador<num2){
+    if(num1 % 3 ===0){
+        console.log (palabra1 + " ,");
+    }else if(num1 % 5 ===0){
+        console.log (palabra2);
+    }else if (num1 %3===0 && num2%5===0){
+        console.log (palabra1+palabra2);
+    }else {
+        (contador)
+    }
+
+}contador++
+
+
+    }
+
+fizzbuzz(hola,adios)
+contarHasta(1,30)
